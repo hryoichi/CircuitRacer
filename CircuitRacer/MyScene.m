8dbe71de55478415d17558dbfb05e4b54617d42e
Ryoichi Hara <rychhr@gmail.com>
2014-06-27 12:19:52 +0900

    Add objects on the scene

--- CircuitRacer/MyScene.m
+++ CircuitRacer/MyScene.m
@@ -8,12 +8,18 @@
 
 #import "MyScene.h"
 
+typedef NS_OPTIONS(NSUInteger, CRPhysicsCategory) {
+    CRBodyCar = 1 << 0,  // 0000001 = 1
+    CRBodyBox = 1 << 1,  // 0000010 = 2
+};
+
 @interface MyScene ()
 
 @property (nonatomic, assign) CRCarType carType;
 @property (nonatomic, assign) CRLevelType levelType;
 @property (nonatomic, assign) NSTimeInterval timeInSeconds;
 @property (nonatomic, assign) NSInteger noOfLabs;
+@property (nonatomic, strong) SKSpriteNode *car;
 
 @end
 
@@ -45,6 +51,18 @@
         sprite;
     });
     [self addChild:track];
+
+    [self p_addCarAtPosition:CGPointMake(CGRectGetMidX(track.frame), 50.0f)];
+
+    // Turn off the world's gravity
+    self.physicsWorld.gravity = CGVectorMake(0.0f, 0.0f);
+    self.physicsBody = ({
+        CGRect frame = CGRectInset(track.frame, 40.0f, 0.0f);
+        SKPhysicsBody *body = [SKPhysicsBody bodyWithEdgeLoopFromRect:frame];
+        body;
+    });
+
+    [self p_addObjectsForTrack:track];
 }
 
 - (void)p_loadLevel {
@@ -56,6 +74,48 @@
 
     NSNumber *laps = level[_levelType - 1][@"laps"];
     _noOfLabs = [laps integerValue];
+}
+
+- (void)p_addCarAtPosition:(CGPoint)startPosition {
+    _car = ({
+        NSString *imageName = [NSString stringWithFormat:@"car_%i", _carType];
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:imageName];
+        sprite.position = startPosition;
+        sprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_car.frame.size];
+        sprite.physicsBody.categoryBitMask    = CRBodyCar;
+        sprite.physicsBody.collisionBitMask   = CRBodyBox;
+        sprite.physicsBody.contactTestBitMask = CRBodyBox;
+        sprite.physicsBody.allowsRotation = NO;
+        sprite;
+    });
+    [self addChild:_car];
+}
+
+
+- (void)p_addBoxAt:(CGPoint)point {
+    SKSpriteNode *box = [SKSpriteNode spriteNodeWithImageNamed:@"box"];
+    box.position = point;
+    box.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:box.size];
+    box.physicsBody.categoryBitMask = CRBodyBox;
+
+    // Simulate friction and prevent the boxes from continuously sliding around
+    box.physicsBody.linearDamping = 1.0f;
+    box.physicsBody.angularDamping = 1.0f;
+
+    [self addChild:box];
+}
+
+- (void)p_addObjectsForTrack:(SKSpriteNode *)track {
+    SKNode *innerBoundary = [SKNode node];
+    innerBoundary.position = track.position;
+    [self addChild:innerBoundary];
+
+    innerBoundary.physicsBody = [SKPhysicsBody
+        bodyWithRectangleOfSize:CGSizeMake(180.0f, 120.0f)];
+    innerBoundary.physicsBody.dynamic = NO;
+
+    [self p_addBoxAt:CGPointMake(track.position.x + 130.0f, track.position.y)];
+    [self p_addBoxAt:CGPointMake(track.position.x - 200.0f, track.position.y)];
 }
 
 @end
