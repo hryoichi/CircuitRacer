5366dbc51a6d6a7e5062d87473b55714aeb19b29
Ryoichi Hara <rychhr@gmail.com>
2014-07-01 11:57:36 +0900

    Add SelectCarViewController

---
+++ CircuitRacer/SelectCarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectCarViewController.h
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/01.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SelectCarViewController : UIViewController
+
+@end
