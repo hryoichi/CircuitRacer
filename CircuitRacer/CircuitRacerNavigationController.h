5a0a942d11350d28220b3237d24c70fdcb178b1b
Ryoichi Hara <rychhr@gmail.com>
2014-07-13 22:22:15 +0900

    Add CircuitRacerNavigationController

---
+++ CircuitRacer/CircuitRacerNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CircuitRacerNavigationController.h
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/13.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CircuitRacerNavigationController : UINavigationController
+
+@end
---
+++ CircuitRacer/CircuitRacerNavigationController.m
@@ -0,0 +1,25 @@
+//
+//  CircuitRacerNavigationController.m
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/13.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "CircuitRacerNavigationController.h"
+
+@interface CircuitRacerNavigationController ()
+
+@end
+
+@implementation CircuitRacerNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
