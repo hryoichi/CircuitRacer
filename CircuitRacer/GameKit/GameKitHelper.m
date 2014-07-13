5323307a52e67737870407e4d856945688b6e96d
Ryoichi Hara <rychhr@gmail.com>
2014-07-13 21:40:24 +0900

    Add GameKitHelper

---
+++ CircuitRacer/GameKit/GameKitHelper.m
@@ -0,0 +1,13 @@
+//
+//  GameKitHelper.m
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/13.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "GameKitHelper.h"
+
+@implementation GameKitHelper
+
+@end
