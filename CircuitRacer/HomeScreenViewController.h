45a2827647d6a9f9c537611cbe63a0435143419c
Ryoichi Hara <rychhr@gmail.com>
2014-07-21 21:25:40 +0900

    Add HomeScreenViewController

---
+++ CircuitRacer/HomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.h
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/21.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HomeScreenViewController : UIViewController
+
+@end
---
+++ CircuitRacer/HomeScreenViewController.m
@@ -0,0 +1,25 @@
+//
+//  HomeScreenViewController.m
+//  CircuitRacer
+//
+//  Created by Ryoichi Hara on 2014/07/21.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HomeScreenViewController.h"
+
+@interface HomeScreenViewController ()
+
+@end
+
+@implementation HomeScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
